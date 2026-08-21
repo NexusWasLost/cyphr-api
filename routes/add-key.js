ba478e2aaba9bf9238d71501ce10cbab2975e43b
Aritra <aritra.2003k@gmail.com>
2026-08-21 15:19:25 +0530

    Added transaction based key insertion to implement limited key storage, updated wrangler.toml accordingly

--- routes/add-key.js
+++ routes/add-key.js
@@ -23,20 +23,32 @@ addkey.post("/add-key", async function (c) {
     const encryptedAPIKeyValue = await encryptKey(c, apiKeyValue);
 
     const sql = neon(c.env.DATABASE_URL);
-    //insert user data
-    await sql.query(`
+
+    //insert user data in one atomic transaction (also check if limit exceeded)
+    const res = await sql.transaction([
+        sql.query(`
             INSERT INTO users(user_id, user_email)
             VALUES($1, $2)
             ON CONFLICT (user_id) DO NOTHING;`,
-        [meta.uid, (meta.email || null)]
-    );
-    //insert api key data
-    const data = await sql.query(`
+            [meta.uid, (meta.email || null)]
+        ),
+        sql.query(`
+            SELECT user_id FROM users WHERE user_id = $1 FOR UPDATE;`,
+            [meta.uid]
+        ),
+        sql.query(`
             INSERT INTO api_keys(user_id, key_name, encrypted_key, key_hint, service_name)
-            VALUES($1, $2, $3, $4, $5)
+            SELECT $1, $2, $3, $4, $5
+            WHERE (SELECT COUNT(*) FROM api_keys WHERE user_id = $1) < $6
             RETURNING key_id;`,
-        [meta.uid, apiKeyName, encryptedAPIKeyValue, keyHint, serviceName]
-    );
+            [meta.uid, apiKeyName, encryptedAPIKeyValue, keyHint, serviceName, c.env.FREE_CAP]
+        )
+    ]);
+
+    //check if the data was inserted
+    if(res[2].length === 0){
+        throw new HTTPException(402, { message: "LIMIT REACHED of free 10 API Keys !" });
+    }
 
     c.executionCtx.waitUntil(c.env.API_CACHE.delete(`keys:${meta.uid}`));
 
@@ -44,7 +56,7 @@ addkey.post("/add-key", async function (c) {
         success: true,
         message: "Key Added Successfully",
         data: {
-            key_id: data[0].key_id,
+            key_id: res[2].key_id,
             key_name: apiKeyName,
             key_hint: keyHint,
             service_name: serviceName
