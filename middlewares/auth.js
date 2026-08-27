e05794671151e66de0137cc81971833c87bd7256
Aritra <aritra.2003k@gmail.com>
2026-08-27 18:40:26 +0530

    Bug fixes: Fixed try-catch block in auth middleware catching downstream errors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,6 +8,7 @@ async function verifyToken(c, token, JWKS) {
     //create a local JWK set from JWKS JSON
     const jsonWebKeySet = createLocalJWKSet(JWKS);
 
+    //auto throws on failed verification
     const content = await jwtVerify(token, jsonWebKeySet, {
         issuer: `https://${c.env.SUPABASE_PROJECT_ID}.supabase.co/auth/v1`,
         audience: "authenticated"
@@ -33,57 +34,71 @@ export const authMiddleware = async function (c, next) {
 
     const token = authHeader.split(" ")[1];
 
+    // CACHE LAYER 1: RAM
+    if (MEMORY_CACHE_JWKS) {
+        try {
+            const user = await verifyToken(c, token, MEMORY_CACHE_JWKS);
+            c.set("meta", user);
+            return await next();
+        }
+        catch (error) {
+            // Stale RAM? Clear it and move to KV/Fetch
+            MEMORY_CACHE_JWKS = null;
+        }
+    }
+
+    // CACHE LAYER 2: KV Cache
+    const kvJWKS = await c.env.API_CACHE.get("JWKS", "json");
+    if (kvJWKS) {
+        try {
+            const user = await verifyToken(c, token, kvJWKS);
+            MEMORY_CACHE_JWKS = kvJWKS; // Sync RAM
+            c.set("meta", user);
+            return await next();
+        }
+        catch (error) {
+            // Stale KV? Don't return next(), let it fall through to fetch
+            console.log("KV JWKS stale, fetching fresh...");
+        }
+    }
+
+    let JWKS_response = null;
     try {
-        // CACHE LAYER 1: RAM
-        if (MEMORY_CACHE_JWKS) {
-            try {
-                const user = await verifyToken(c, token, MEMORY_CACHE_JWKS);
-                c.set("meta", user);
-                return await next();
-            }
-            catch (e) {
-                // Stale RAM? Clear it and move to KV/Fetch
-                MEMORY_CACHE_JWKS = null;
-            }
-        }
-
-        // CACHE LAYER 2: KV Cache
-        const kvJWKS = await c.env.API_CACHE.get("JWKS", "json");
-        if (kvJWKS) {
-            try {
-                const user = await verifyToken(c, token, kvJWKS);
-                MEMORY_CACHE_JWKS = kvJWKS; // Sync RAM
-                c.set("meta", user);
-                return await next();
-            }
-            catch (e) {
-                // Stale KV? Don't return next(), let it fall through to fetch
-                console.log("KV JWKS stale, fetching fresh...");
-            }
-        }
-
         //fetch JSON Web Key Set
-        const JWKS_response = await fetch(`https://${c.env.SUPABASE_PROJECT_ID}.supabase.co/auth/v1/.well-known/jwks.json`, {
+        JWKS_response = await fetch(`https://${c.env.SUPABASE_PROJECT_ID}.supabase.co/auth/v1/.well-known/jwks.json`, {
             method: "GET"
         });
-        if (!JWKS_response.ok) throw new Error("Failed to fetch fresh JWKS...");
-
-        const freshJWKS = await JWKS_response.json();
-        //cache in isolate memory
-        MEMORY_CACHE_JWKS = freshJWKS;
-        //cache the JWKS in KV
-        c.executionCtx.waitUntil(c.env.API_CACHE.put("JWKS", JSON.stringify(freshJWKS), {
-            expirationTtl: 600
-        }));
-
-        const customPayload = await verifyToken(c, token, freshJWKS);
-        c.set("meta", customPayload);
-        await next();
+        if (!JWKS_response.ok) {
+            console.log("Failed to fetch fresh JWKS...");
+            throw new Error("Failed to fetch fresh JWKS...");
+        }
     }
     catch (error) {
         throw new HTTPException(401, {
-            message: "Authentication failed",
+            message: "Failed to fetch fresh JWKS, Try again later !",
             cause: error
         });
     }
+
+    const freshJWKS = await JWKS_response.json();
+    //cache in isolate memory
+    MEMORY_CACHE_JWKS = freshJWKS;
+    //cache the JWKS in KV
+    c.executionCtx.waitUntil(c.env.API_CACHE.put("JWKS", JSON.stringify(freshJWKS), {
+        expirationTtl: 600
+    }));
+
+    let customPayload = null;
+    try{
+        customPayload = await verifyToken(c, token, freshJWKS);
+    }
+    catch(error){
+        throw new HTTPException(401, {
+            message: "Authentication failed, Please login again !",
+            cause: error
+        });
+    }
+
+    c.set("meta", customPayload);
+    await next();
 }
